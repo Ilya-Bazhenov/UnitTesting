364b7c51dfb21c4eda3852ee6bca90152c90c9f8
Ilya Bazhenov <i1.bazhenov@yandex.ru>
2021-07-06 11:46:33 +0300

    added simple calculator class

---
+++ FirstUnitTestingTasks/src/calculator/SimpleCalculator.java
@@ -0,0 +1,21 @@
+package calculator;
+
+public class SimpleCalculator {
+    private SimpleCalculator() {}
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+
+    public static int mul(int a, int b) {
+        return a * b;
+    }
+
+    public static int div(int a, int b) {
+        return a / b;
+    }
+}
